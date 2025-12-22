6c5beb74f32ba9d70b115e51e55d27201b911dc5
Edik Krmakov <krmakov2001@mail.ru>
2025-12-23 00:38:41 +0300

    attempt 2

--- src/main/java/com/remaslover/telegrambotaq/config/WebConfig.java
+++ src/main/java/com/remaslover/telegrambotaq/config/WebConfig.java
@@ -1,6 +1,8 @@
 package com.remaslover.telegrambotaq.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -8,24 +10,33 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${server.servlet.context-path:/}")
+    private String contextPath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/**")
-                .addResourceLocations("classpath:/static/")
-                .addResourceLocations("classpath:/public/");
-
-        registry.addResourceHandler("/css/**")
-                .addResourceLocations("classpath:/static/css/");
-
-        registry.addResourceHandler("/js/**")
-                .addResourceLocations("classpath:/static/js/");
-
-        registry.addResourceHandler("/assets/**")
-                .addResourceLocations("classpath:/static/assets/");
+        registry.addResourceHandler(
+                "/css/**",
+                "/js/**",
+                "/assets/**",
+                "/static/**"
+        ).addResourceLocations(
+                "classpath:/static/css/",
+                "classpath:/static/js/",
+                "classpath:/static/assets/",
+                "classpath:/static/"
+        );
     }
 
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
+        registry.addViewController("/index").setViewName("home");
+        registry.addViewController("/home").setViewName("home");
+    }
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+        configurer.enable();
     }
 }
